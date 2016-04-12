0a8bb702aad941c8e0303fecd4ab3f12d0b0c4fe
Taylor Pokoj <pokojt@gmail.com>
2016-04-11 18:57:16 -0700

    Add sub components and render in HTML

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,59 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+/*  App */
+
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div className="catch-of-the-day">
+      <div className="menu">
+        <Header />
+      </div>
+      <Order/>
+      <Inventory/>
+      </div>
+    )
+  }
+});
+
+/* Header <Header/> */
+
+var Header = React.createClass({
+  render : function() {
+    return (
+      <p>Header</p>
+    )
+  }
+});
+
+/* Order <Order/>*/
+
+var Order = React.createClass({
+  render : function() {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/* Inventory <Inventory/> */
+
+var Inventory = React.createClass({
+  render : function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+})
+
 /* Store picker component. This will let us make <StorePicker/> to use anywhere on page */
 
 var StorePicker = React.createClass({
 
   render: function() {
-    var name = "Taylor";
+    var name = "Dude";
     return (
       <form className="store-selector">
         <h2>Please Enter A Store {name}</h2>
@@ -18,4 +66,4 @@ var StorePicker = React.createClass({
 
 /* Code to render component in html */
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));

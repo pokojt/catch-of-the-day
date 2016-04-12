f2ffe2acc60089a35c57c89a28ae6c030ece1526
Taylor Pokoj <pokojt@gmail.com>
2016-04-11 18:45:03 -0700

    Add code for first component store picker

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+/* Store picker component. This will let us make <StorePicker/> to use anywhere on page */
 
+var StorePicker = React.createClass({
+
+  render: function() {
+    var name = "Taylor";
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store {name}</h2>
+        <input type="text" ref="storeId" required/>
+        <input type="submit"/>
+      </form>
+    )
+  }
+});
+
+/* Code to render component in html */
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
